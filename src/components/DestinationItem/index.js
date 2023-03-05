7b678e976716d6adf37c56a5e9503385c6424dfb
Rakesh <mandarakesh151@gmail.com>
2023-03-05 16:14:02 +0000

    first commit

---
+++ src/components/DestinationItem/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+
+const DestinationItem = props => {
+  const {userDetails} = props
+  const {name, imgUrl} = userDetails
+
+  return (
+    <li className="list-items">
+      <img alt={name} className="image" src={imgUrl} />
+      <p className="names">{name}</p>
+    </li>
+  )
+}
+export default DestinationItem
